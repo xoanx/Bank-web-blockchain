fa8459eeb1d644d0644e131156d685579fb56161
xoanx <snowblack1352k2@gmail.com>
2025-08-16 22:00:24 +0700

    updated

--- security/src/main/java/com/example/security/controller/AuthController.java
+++ security/src/main/java/com/example/security/controller/AuthController.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
